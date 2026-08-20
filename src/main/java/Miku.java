610cf45c66546acfe985f24d2e61c4afff170de0
Hoang Hai <39663233+Kubogi@users.noreply.github.com>
2026-08-20 16:13:11 +0800

    mark & unmark tasks

--- src/main/java/Miku.java
+++ src/main/java/Miku.java
@@ -1,9 +1,6 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-/**
- * The entry point for the Miku chatbot.
- */
 public class Miku {
     public static void main(String[] args) {
         String separator = "____________________________________________________________";
@@ -20,23 +17,35 @@ public class Miku {
         System.out.println(separator);
 
         Scanner scanner = new Scanner(System.in);
-        ArrayList<String> tasks = new ArrayList<>();
+        ArrayList<Task> tasks = new ArrayList<>();
         while (scanner.hasNextLine()) {
             String command = scanner.nextLine();
+            String[] cmdArgs = command.trim().split("\\s+");
             System.out.println(separator);
 
-            if (command.equals("bye")) {
+            if (cmdArgs[0].equals("bye")) {
                 System.out.println("Bye bye! Hope to see you again soon! ☆");
                 System.out.println(separator);
                 break;
-            } else if (command.equals("list")) {
+            } else if (cmdArgs[0].equals("list")) {
                 System.out.println("Tasks:");
                 for (int i = 0; i < tasks.size(); i++) {
                     System.out.println(i+1 + ". " + tasks.get(i));
                 }
+            } else if (cmdArgs[0].equals("mark")) {
+                // assuming it's a in-bounds integer
+                Task task = tasks.get(Integer.parseInt(cmdArgs[1]) - 1);
+                task.markAsDone();
+                System.out.println("Okay ☆ I've marked this task as done!");
+                System.out.println(task);
+            } else if (cmdArgs[0].equals("unmark")) {
+                Task task = tasks.get(Integer.parseInt(cmdArgs[1]) - 1);
+                task.unmarkDone();
+                System.out.println("Oops... I've marked this task as not done yet:");
+                System.out.println(task);
             } else {
                 System.out.println("Added: " + command + " ☆");
-                tasks.add(command);
+                tasks.add(new Task(command));
             }
 
             System.out.println(separator);
