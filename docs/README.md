a1359fc1c84836fcd49b65e565757d97e532157c
Hoang Hai <39663233+Kubogi@users.noreply.github.com>
2026-08-20 15:35:12 +0800

    new name

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Paimon User Guide
+﻿# Miku User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Miku.java
@@ -0,0 +1,20 @@
+/**
+ * The entry point for the Miku chatbot.
+ */
+public class Miku {
+    public static void main(String[] args) {
+        String separator = "____________________________________________________________";
+        String banner = " __  __ _ _          \n"
+                + "|  \\/  (_) | ___   _ \n"
+                + "| |\\/| | | |/ / | | |\n"
+                + "| |  | | |   <| |_| |\n"
+                + "|_|  |_|_|_|\\_\\\\__,_|\n";
+        System.out.println(separator);
+        System.out.println(banner);
+        System.out.println("Hello! I'm Miku.");
+        System.out.println("What can I do for you?");
+        System.out.println(separator);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(separator);
+    }
+}
