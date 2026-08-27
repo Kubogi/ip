e0834dbfc0faa14390e3b80a336e7dff7d910c9b
Hoang Hai <39663233+Kubogi@users.noreply.github.com>
2026-08-27 23:03:12 +0800

    Document exit command behavior

--- src/main/java/miku/command/ExitCommand.java
+++ src/main/java/miku/command/ExitCommand.java
@@ -6,11 +6,13 @@ import miku.ui.Ui;
 
 /** Ends the current Miku session after showing a farewell message. */
 public class ExitCommand extends Command {
+    /** Displays the farewell message. */
     @Override
     public void execute(TaskList tasks, Ui ui, Storage storage) {
         ui.showGoodbye();
     }
 
+    /** Indicates that this command ends the application session. */
     @Override
     public boolean isExit() {
         return true;
